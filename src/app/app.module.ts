772ad53b42a4cfcdf40e0dd7ea4171ec831a2d01
Juan Sebastian Palacios Cortes <juansebastianpalacioscortes@iMac-de-Juan.local>
2020-11-10 18:33:02 -0500

    empty arquitecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {appReducers} from './store/app.reducer';
+import {effectsArray} from './store/effects';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment.prod';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    // modulos NgRx
+    // este modulo instancia y carga los reducers que esten en appReducers.
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot(effectsArray),
+    // modulo para herramientas de desarrollo.
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/app.reducer.ts
@@ -0,0 +1,6 @@
+import {ActionReducerMap} from '@ngrx/store';
+
+
+
+export const appReducers: ActionReducerMap<any> = {
+};
